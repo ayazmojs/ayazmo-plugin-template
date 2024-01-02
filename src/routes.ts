ccd4802a201e6eaebb5adf25fb89a918206aed26
Tsvetan Nikolov <tsvetan.nikolov@coliquio.de>
2024-01-02 18:54:04 +0200

    renamed route

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,12 @@ import { AyazmoRouteOptions, AyazmoRequest, AyazmoReply } from '@ayazmo/types';
 const routes: AyazmoRouteOptions[] = [
   {
     method: 'GET',
-    url: '/users',
+    url: '/pets',
     handler: async (request: AyazmoRequest, reply: AyazmoReply) => {
 
       // get the user service from the DI container
       const petsService = request.diScope.resolve('petsService');
-      reply.code(200).send({ pets: petsService.getAll() });
+      reply.code(200).send({ pets: await petsService.getAll() });
 
     }
   },
