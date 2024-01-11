d41012cc05b500419df9ee178608c3f82238f380
Tsvetan Nikolov <tsvetan.nikolov@coliquio.de>
2024-01-11 14:22:03 +0200

    comment

--- src/entities/Pet.ts
+++ src/entities/Pet.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+// import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 /**
  * Defining Entity - Example Template
@@ -9,40 +9,40 @@ import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
  * For more information, see https://mikro-orm.io/docs/defining-entities.
  */
 
-@Entity()
-export class Pet {
+// @Entity()
+// export class Pet {
 
-  @PrimaryKey()
-  id!: number;
+//   @PrimaryKey()
+//   id!: number;
 
-  @Property()
-  name!: string;
+//   @Property()
+//   name!: string;
 
-  @Property()
-  type!: string; // e.g., 'Dog', 'Cat'
+//   @Property()
+//   type!: string; // e.g., 'Dog', 'Cat'
 
-  @Property({ nullable: true })
-  breed?: string;
+//   @Property({ nullable: true })
+//   breed?: string;
 
-  @Property()
-  age!: number;
+//   @Property()
+//   age!: number;
 
-  @Property({ type: 'text', nullable: true })
-  bio?: string;
+//   @Property({ type: 'text', nullable: true })
+//   bio?: string;
 
-  @Property({ onCreate: () => new Date() })
-  createdAt: Date = new Date();
+//   @Property({ onCreate: () => new Date() })
+//   createdAt: Date = new Date();
 
-  @Property({ onUpdate: () => new Date(), nullable: true })
-  updatedAt: Date = new Date();
+//   @Property({ onUpdate: () => new Date(), nullable: true })
+//   updatedAt: Date = new Date();
 
-  // Additional properties or relations can be added here as per your requirements
+//   // Additional properties or relations can be added here as per your requirements
 
-  constructor(name: string, type: string, age: number, breed?: string, bio?: string) {
-    this.name = name;
-    this.type = type;
-    this.age = age;
-    this.breed = breed;
-    this.bio = bio;
-  }
-}
+//   constructor(name: string, type: string, age: number, breed?: string, bio?: string) {
+//     this.name = name;
+//     this.type = type;
+//     this.age = age;
+//     this.breed = breed;
+//     this.bio = bio;
+//   }
+// }
